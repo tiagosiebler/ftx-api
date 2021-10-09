2aef84bb2da405de51c2861f587927ea5c54fbda
print (null) <palashdn@zoho.com>
2021-10-09 17:15:03 +0530

    return browserMethods

--- src/util/node-support.ts
+++ src/util/node-support.ts
@@ -2,13 +2,12 @@ import { createHmac } from 'crypto';
 import * as browserMethods from './browser-support';
 
 export async function signMessage(message: string, secret: string): Promise<string> {
-   if (typeof createHmac === 'function') { 
-  return createHmac('sha256', secret)
-    .update(message)
-    .digest('hex');
-}
-   
-   return browserMethods.signMessage(message, secret);
+  if (typeof createHmac === 'function') {
+    return createHmac('sha256', secret)
+      .update(message)
+      .digest('hex');
+  }
+  return browserMethods.signMessage(message, secret);
 };
 
 export async function signWsAuthenticate(timestamp: number, secret: string): Promise<string> {
