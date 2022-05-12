ac70a1b112046bb603c24624b35fb48776d4b365
Chris <ccwijnia@gmail.com>
2022-05-12 15:29:09 +0200

    move response types to folder

--- src/types/rest/index.ts
+++ src/types/rest/index.ts
@@ -1,19 +1,6 @@
-import { ConditionalOrderType, ConditionalOrderTypeNoUnderscore, OrderSide, OrderType } from "./orders";
+import { ConditionalOrderType, ConditionalOrderTypeNoUnderscore, OrderSide, OrderType } from "./response/orders";
 
-export * from "./api-key-info";
-export * from "./nft";
-export * from "./staking";
-export * from "./subaccount";
-export * from "./markets";
-export * from "./futures";
-export * from "./account";
-export * from "./wallet";
-export * from "./orders";
-export * from "./quote";
-export * from "./options";
-export * from "./spot-margin";
-export * from "./fills";
-export * from "./leveraged-tokens";
+export * from "./response";
 
 export interface TimeRange {
   start_time?: number;
